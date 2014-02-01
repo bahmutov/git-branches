254ab8449d77d914a707875984134e486ec7484b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-01 17:55:44 -0500

    working

--- branches.sh
+++ branches.sh
@@ -2,8 +2,11 @@
 
 branches=`git branch --list`
 
-while read -r line; do
-    echo "$line"
-    description=`git config branch.$line.description`
+while read -r branch; do
+  # git marks current branch with "* ", remove it
+  clean_branch_name=${branch//\*/}
+  clean_branch_name=${clean_branch_name//\ /}
+  description=`git config branch.$clean_branch_name.description`
+  echo "$branch $description"
 done <<< "$branches"
 
