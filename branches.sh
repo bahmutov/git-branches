b7fc39ee4b3f53b60c7c19b9f4d79b4135e546ed
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-02 17:01:30 -0500

    comments

--- branches.sh
+++ branches.sh
@@ -3,6 +3,17 @@
 branch=""
 branches=`git branch --list`
 
+# requires git > v.1.7.9
+
+# you can set branch's description using command
+# git branch --edit-description
+# this opens the configured text editor, enter message, save and exit
+# if one editor does not work (for example Sublime does not work for me)
+# try another one, like vi
+
+# you can see branch's description using
+# git config branch.<branch name>.description
+
 while read -r branch; do
   # git marks current branch with "* ", remove it
   clean_branch_name=${branch//\*\ /}
