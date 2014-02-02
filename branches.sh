3161a3cd392864f4f8835cb425d1d5a1bbed020c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-02 16:59:04 -0500

    more spacing

--- branches.sh
+++ branches.sh
@@ -7,6 +7,6 @@ while read -r branch; do
   # git marks current branch with "* ", remove it
   clean_branch_name=${branch//\*\ /}
   description=`git config branch.$clean_branch_name.description`
-  printf "%-10s %s\n" "$branch" "$description"
+  printf "%-15s %s\n" "$branch" "$description"
 done <<< "$branches"
 
